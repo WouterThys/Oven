d8138ed931e7e4a3902885b41c3dbd3b98d990f6
Wouter Thys <thys_wouter@hotmail.com>
2016-11-20 16:45:39 +0100

    added graphPlotter.h

---
+++ Utils/graphPlotter.h
@@ -0,0 +1,21 @@
+#ifndef GRAPHPLOTTER_H
+#define	GRAPHPLOTTER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+#include <stdint.h>
+#include <stdbool.h>
+
+    
+// FUNCTIONS
+
+void drawAxis(const char *xUnits, const char *yUnits);
+    
+    
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	
