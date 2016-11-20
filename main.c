6e9a5d071a14def3306304822359d77b99bbb646
Wouter Thys <thys_wouter@hotmail.com>
2016-11-20 15:54:55 +0100

    back on master?

--- main.c
+++ main.c
@@ -25,30 +25,9 @@ void main(void) {
     D_PWM_Init(PWM_MODULE_1);
     D_PWM_Enabele(PWM_MODULE_1, true);
     D_PWM_SetDuty(PWM_MODULE_1, (0xAD + (0xFF-0xAD)*5/8));
-    
-    uint8_t i = 0;
-    uint8_t j = 0;
-//    for (i=0; i < 192; i++) {
-//        j++;
-//        if (j == 63){
-//            j = 0;
-//        }
-//        D_GLCD_DrawDot(i,j,GLCD_WHITE);
-//    }
-    
-    while(i>=0) {
-        j++;
-        if (j == 64) {
-            j = 0;
-        }
-        D_GLCD_DrawDot(i,j,GLCD_WHITE);
-        i--;
-    }
-    
-    
+   
     while(1) {
         
-        //PORTAbits.RA1 = !PORTAbits.RA1;
     }
     return;
 }
